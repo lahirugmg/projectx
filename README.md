90d48f3b9ef6ae7e7be43b590ff634d2d3a2569d
Lahiru Gamage <lahirugmg@gmail.com>
2019-11-23 00:13:43 -0600

    Set HTML content from python service response

--- README.md
+++ README.md
@@ -7,6 +7,5 @@ Python backend server
 - Start the server inside projectx directory
 `python app.py`
 - Invoke service
-`curl -vX GET http://localhost:5050/ `
-- References:
-https://medium.com/@umerfarooq_26378/web-services-in-python-ef81a9067aaf
+`curl -vX GET http://localhost:5050/getstudents `
+- Test webpage 'student.html'
--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-@app.route('/getinfo')
+@app.route('/getstudents')
 def index():
-    return jsonify({"studentName":"John","studentId":"23432"}), 200
+    return jsonify({"students":[{"studentName":"John Botha","studentId":"23432"},{"studentName":"Nickalas Cage","studentId":"90098"},{"studentName":"Kasun Pathirana","studentId":"334321"}]}), 200
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)
 
