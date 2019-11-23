1ec3b10bc3679991c7c212b3686f62c8d46296a5
Lahiru Gamage <lahirugmg@gmail.com>
2019-11-22 23:23:25 -0600

    Add JSON response

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
-@app.route('/')
+@app.route('/getinfo')
 def index():
-    return "Hello, World!"
+    return jsonify({"studentName":"John","studentId":"23432"}), 200
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)
+
